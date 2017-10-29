d08b4cd11e923018d3450340aaaad526ee0de618
bsormagec <bsormagec@gmail.com>
2017-10-29 23:26:19 +0300

    fix

--- start.sh
+++ start.sh
@@ -17,11 +17,11 @@ fi
 # ------------------------------------------
 
 if [[ "$PRODUCTION" == "1" ]] || [[ "$IN_PRODUCTION" == "1" ]]; then
-    echo php_flag[display_errors] = off >> /etc/php7/php-fpm.conf
-    echo log_level = warning >> /etc/php7/php-fpm.conf
-    sed -i "s/expose_php = On/expose_php = Off/g" /etc/php7/conf.d/php.ini
+    echo php_flag[display_errors] = off >> /etc/php7.1/php-fpm.conf
+    echo log_level = warning >> /etc/php7.1/php-fpm.conf
+    sed -i "s/expose_php = On/expose_php = Off/g" /etc/php7.1/conf.d/php.ini
 else
-    echo php_flag[display_errors] = on >> /etc/php7/php-fpm.conf
+    echo php_flag[display_errors] = on >> /etc/php7.1/php-fpm.conf
 fi
 
 # ------------------------------------------
@@ -29,7 +29,7 @@ fi
 # ------------------------------------------
 
 if [ ! -z "$PHP_MEMORY_LIMIT" ]; then
-    sed -i "s/memory_limit = 128M/memory_limit = ${PHP_MEMORY_LIMIT}M/g" /etc/php7/conf.d/php.ini
+    sed -i "s/memory_limit = 128M/memory_limit = ${PHP_MEMORY_LIMIT}M/g" /etc/php7.1/conf.d/php.ini
 fi
 
 # ------------------------------------------
@@ -37,7 +37,7 @@ fi
 # ------------------------------------------
 
 if [ ! -z "$PHP_POST_MAX_SIZE" ]; then
-    sed -i "s/post_max_size = 100M/post_max_size = ${PHP_POST_MAX_SIZE}M/g" /etc/php7/conf.d/php.ini
+    sed -i "s/post_max_size = 100M/post_max_size = ${PHP_POST_MAX_SIZE}M/g" /etc/php7.1/conf.d/php.ini
 fi
 
 # ------------------------------------------
@@ -45,7 +45,7 @@ fi
 # ------------------------------------------
 
 if [ ! -z "$PHP_UPLOAD_MAX_FILESIZE" ]; then
-    sed -i "s/upload_max_filesize = 100M/upload_max_filesize= ${PHP_UPLOAD_MAX_FILESIZE}M/g" /etc/php7/conf.d/php.ini
+    sed -i "s/upload_max_filesize = 100M/upload_max_filesize= ${PHP_UPLOAD_MAX_FILESIZE}M/g" /etc/php7.1/conf.d/php.ini
 fi
 
 # ------------------------------------------
@@ -53,18 +53,18 @@ fi
 # ------------------------------------------
 
 if [ ! -z "$PHP_MAX_EXECUTION_TIME" ]; then
-    sed -i "s/max_execution_time = 30/max_execution_time = ${PHP_MAX_EXECUTION_TIME}/g" /etc/php7/conf.d/php.ini
+    sed -i "s/max_execution_time = 30/max_execution_time = ${PHP_MAX_EXECUTION_TIME}/g" /etc/php7.1/conf.d/php.ini
 fi
 
-sed -i "s/pm.max_children = 5/pm.max_children = 70/g" /etc/php7/php-fpm.d/www.conf
-sed -i "s/pm.start_servers = 3/pm.start_servers = 20/g" /etc/php7/php-fpm.d/www.conf
-sed -i "s/pm.min_spare_servers = 2/pm.min_spare_servers = 20/g" /etc/php7/php-fpm.d/www.conf
-sed -i "s/pm.max_spare_servers = 4/pm.max_spare_servers = 35/g" /etc/php7/php-fpm.d/www.conf
-sed -i "s/;pm.max_requests = 200/pm.max_requests = 0/g" /etc/php7/php-fpm.d/www.conf
+sed -i "s/pm.max_children = 5/pm.max_children = 70/g" /etc/php7.1/php-fpm.d/www.conf
+sed -i "s/pm.start_servers = 3/pm.start_servers = 20/g" /etc/php7.1/php-fpm.d/www.conf
+sed -i "s/pm.min_spare_servers = 2/pm.min_spare_servers = 20/g" /etc/php7.1/php-fpm.d/www.conf
+sed -i "s/pm.max_spare_servers = 4/pm.max_spare_servers = 35/g" /etc/php7.1/php-fpm.d/www.conf
+sed -i "s/;pm.max_requests = 200/pm.max_requests = 0/g" /etc/php7.1/php-fpm.d/www.conf
 chown -R nginx:nginx $webroot
 
 
-# /etc/php7/php-fpm.d/www.conf
+# /etc/php7.1/php-fpm.d/www.conf
 # CHANGE PHP SETTINGS FOR
 # pm.max_children = 70
 # pm.start_servers = 20
